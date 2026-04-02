05df573e38ee160776d26d9bbfac98f2df496bee
softdev-nic <softtechon.inc@gmail.com>
2026-04-02 18:35:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+ 
+function App() {
+  
+
+  return (
+    <>
+      <div className="h-screen flex iten-center justify-center bg-gray-100">
+        <h1 className="text-4xl">hello</h1>
+        </div> 
+    </>
+  )
+}
+
+export default App
